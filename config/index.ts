65b9bd528d9ad1c519af676cc3661afef82394f2
voidsatisfaction <lourie@naver.com>
2018-10-25 17:07:34 +0900

    chore: add line-bot-accesstoken and secret token on configure

--- config/index.ts
+++ config/index.ts
@@ -4,6 +4,8 @@ export class Configure {
   readonly dbPassword: string
   readonly dbHost: string
   readonly dbPort: number
+  readonly lineBotAccessToken: string
+  readonly lineBotSecretToken: string
 
   constructor() {
     switch (process.env.NODE_ENV) {
@@ -16,6 +18,9 @@ export class Configure {
         this.dbPassword = ''
         this.dbHost = 'localhost'
         this.dbPort = 13306
+
+        this.lineBotAccessToken = process.env.LineBotAccessToken
+        this.lineBotSecretToken = process.env.LineBotSecretToken
         break
       case 'TEST':
         this.dbName = 'Hiyoko_core'
@@ -23,6 +28,9 @@ export class Configure {
         this.dbPassword = ''
         this.dbHost = 'localhost'
         this.dbPort = 13306
+
+        this.lineBotAccessToken = process.env.LineBotAccessToken
+        this.lineBotSecretToken = process.env.LineBotSecretToken
         break
       default:
         throw `NODE_ENV environment variable is not valid value NODE_ENV: ${process.env.NODE_ENV}`
