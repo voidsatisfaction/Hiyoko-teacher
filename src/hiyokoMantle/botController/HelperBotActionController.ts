97420f399f51e6edefe875759620592d353ba67c
voidsatisfaction <lourie@naver.com>
2018-12-10 22:42:57 +0900

    fix: helper message

--- src/hiyokoMantle/botController/HelperBotActionController.ts
+++ src/hiyokoMantle/botController/HelperBotActionController.ts
@@ -2,18 +2,38 @@ import { BotActionControllerBase } from './Base'
 import { BotAction } from '../model/botAction'
 import { BotActionResult, BotActionResultCreator } from '../model/botActionResult'
 import { Context } from '../web/bot/context'
+import { Configure } from '../../../config';
 
 export class HelperBotActionController extends BotActionControllerBase {
   async execute(context: Context, botAction: BotAction): Promise<BotActionResult> {
-    return BotActionResultCreator.textMessage(`
-      This is not a valid input.
+    const config = new Configure()
+    switch (config.nodeEnv) {
+      case 'PROD':
+        return BotActionResultCreator.textMessage(`
+          This is not a valid input.
 
-      You can use either menu located below or links as follows
+          You can use either menu located below or links as follows
 
-      -------
-      Adding vocabulary: line://app/1615960675-Gye0WZOX
-      Seeing vocabulary Lists: line://app/1615960675-d85ko9yN
-      -------
-    `)
+          -------
+          Adding vocabulary: line://app/1627693639-bGX5kagk
+          Seeing vocabulary Lists: line://app/1627693639-YjQOy8ey
+          Taking Quizzes: line://app/1627693639-YbVm2Pz2
+          -------
+        `)
+      case 'DEV':
+        return BotActionResultCreator.textMessage(`
+          This is not a valid input.
+
+          You can use either menu located below or links as follows
+
+          -------
+          Adding vocabulary: line://app/1615960675-Gye0WZOX
+          Seeing vocabulary Lists: line://app/1615960675-d85ko9yN
+          Taking Quizzes: line://app/1615960675-6DPkpxjX
+          -------
+        `)
+      default:
+        break;
+    }
   }
 }
