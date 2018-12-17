65a9a6250a230816cb9d9afaac196d68639eca5c
voidsatisfaction <lourie@naver.com>
2018-12-17 18:11:26 +0900

    feat: add materialMessageConverter

---
+++ src/hiyokoWorker/materialMessageConverter/README.md
@@ -0,0 +1,10 @@
+# Material Message Converter
+
+## How to use
+
+```sh
+# 1. Create json file with template.json structure
+
+# 2. Command
+node createPushMessageJSON.js {createdFile}
+```
---
+++ src/hiyokoWorker/materialMessageConverter/createPushMessageJSON.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const fs = require('fs')
+
+function compose(...fns) {
+  return function(arg) {
+    return fns.reduce((res, fn) => fn(res), arg)
+  }
+}
+
+function parseRawLocalJSON({ location }) {
+  const jsonData = require(path.join(__dirname, location))
+
+  return ({ jsonData })
+}
+
+function toPushMessageString({ jsonData }) {
+  const { Date, VideoURL, TranscriptURL, Themes } = jsonData
+
+  const message = `
+${Date}
+=====Material URLs=====
+Video: ${VideoURL}
+Transcript: ${TranscriptURL}
+=====Video Themes======
+${Themes.reduce((str, themeData) => str + `${themeData.Time}: ${themeData.Theme}\n`, '')}
+  `
+
+  return ({ jsonData, message: message.trim() })
+}
+
+function savePushMessageJSON({ jsonData, message }) {
+  const target = { message }
+  const jsonString = JSON.stringify(target).trim()
+  fs.writeFile(path.join(__dirname, `/pushMessageJSON/${jsonData.Date}.json`), jsonString, 'utf8', (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(`${message}`)
+  })
+}
+
+const createPushMessageJSON = compose(
+  parseRawLocalJSON,
+  toPushMessageString,
+  savePushMessageJSON
+)
+
+const argv = process.argv
+const location = argv[2]
+if (!location) {
+  throw `location is not set`
+}
+
+createPushMessageJSON({ location })
