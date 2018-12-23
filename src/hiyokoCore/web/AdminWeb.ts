db4550dcdc89e5d9715be85ae1d869f982d38b89
voidsatisfaction <lourie@naver.com>
2018-12-24 01:06:57 +0900

    feat: add /admin/users/countSummaries

--- src/hiyokoCore/web/AdminWeb.ts
+++ src/hiyokoCore/web/AdminWeb.ts
@@ -2,6 +2,9 @@ import * as express from 'express'
 
 import { UserApplication } from '../application/UserApplication'
 import { checkAdminTokenMiddleware } from './middleware/AdminMiddleware'
+import { PlanningApplication } from '../application/PlanningApplication';
+import { CountCategory } from '../domain/model/CountSummary';
+import { DateString } from '../../util/DateTime';
 
 const AdminRouter = express.Router()
 
@@ -24,6 +27,24 @@ AdminRouter.get('/users/all', async (req: express.Request, res: express.Response
     const allUsers = await userApplication.adminListAll()
 
     res.json({ users: allUsers })
+  } catch(e) {
+    res.status(500).json({ error: e.toString() })
+  }
+})
+
+AdminRouter.get('/users/countSummaries', async (req: express.Request, res: express.Response) => {
+  try {
+    const userIds: string[] = req.query.userId
+    const countCategory: CountCategory = req.query.countCategory
+    const date: DateString = req.query.date
+
+    const planningApplication = new PlanningApplication(SYSTEM_USER_ID)
+
+    const countPlans = await planningApplication.adminGetCountPlansByUserIdsAndCountCategoryAndDate(
+      userIds, countCategory, date
+    )
+
+    res.json(countPlans)
   } catch(e) {
     res.status(500).json({ error: e.toString() })
   }
--- test/hiyokoCore/web/AdminWeb.test.ts
+++ test/hiyokoCore/web/AdminWeb.test.ts
@@ -1,6 +1,9 @@
 import * as request from 'supertest'
+import { expect } from 'chai'
 
 import app from '../../../src/hiyokoCore/server'
+import { CountCategory } from '../../../src/hiyokoCore/domain/model/CountSummary';
+import { DateTime } from '../../../src/util/DateTime';
 
 describe('/admin', () => {
   it('should not accessed without admin-token', (done) => {
@@ -25,6 +28,30 @@ describe('/admin', () => {
         .set('Accept', 'application/json')
         .set('admin-token', '123')
         .expect(200)
+    })
+  })
+
+  describe('GET /admin/users/countSummaries', () => {
+    before(async () => {
+      await request(app)
+        .post('/users')
+        .send({ userId: '123123123', productId: 123 })
+        .set('Accept', 'application/json')
+    })
+
+    it('should get all countSummaries', async () => {
+      const res = await request(app)
+        .get('/admin/users/countSummaries?userId[]=123123123')
+        .set('Accept', 'application/json')
+        .set('admin-token', '123')
+        .query({
+          countCategory: CountCategory.addingVocabularyList,
+          date: new DateTime().toDateString()
+        })
+        .expect(200)
+
+      expect(res.body[0].userId).to.be.equal('123123123')
+      expect(res.body[0].countSummary).to.be.equal(null)
     })
   })
 })
